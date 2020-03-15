a82cfe6f1552996196f21bc04582256e42deca44
jakob <jakob.schoser@aerodelft.nl>
2020-03-15 20:36:40 +0100

    Integrated GUI with plot generation

--- Master program/master.py
+++ Master program/master.py
@@ -1,102 +1,33 @@
 import tkinter as tk
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
-# Implement the default Matplotlib key bindings.
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.figure import Figure
+from matplotlib import pyplot as plt
 import numpy as np
 import cartopy.crs as ccrs
 from GUI import Select_pollutant
-"""
-def open_file():
-    """Open a file for editing."""
-    filepath = askopenfilename(
-        filetypes=[("Text Files", "*.nc4"), ("All Files", "*.*")])
 
-    return filepath
-"""
-def generate_plot(dataset, variable, level, time):
-    da = getattr(getattr(getattr(dataset, variable), "sel")(lev=level, method='nearest'), "sel")(time='2005-1-17')
+
+def show_plot(dataset, variable, level, time):
+    da = getattr(getattr(getattr(dataset, variable), "sel")(lev=level, method='nearest'), "sel")(time=time)
     proj = ccrs.PlateCarree()
-    fig = Figure(figsize=(5, 4), dpi=100)
 
     # Create axes and add map
+    fig = plt.subplots()
     ax = fig.axes(projection=proj)  # create axes
     ax.coastlines(resolution='50m')  # draw coastlines with given resolution
 
     # Set color and scale of plot
-    cax = da[0, :, :].plot(add_colorbar=True,
-                           cmap='coolwarm',
-                           vmin=da.values.min(),
-                           vmax=da.values.max(),
-                           cbar_kwargs={'extend': 'neither'})
+    da[0, :, :].plot(add_colorbar=True, cmap='coolwarm', vmin=da.values.min(), vmax=da.values.max(),
+                     cbar_kwargs={'extend': 'neither'})
 
-    return fig
+    plt.show()
 
 
-""""
-filepath = open_file()
+running = True
 
+while running:
+    filepath, DS = Select_pollutant()
+    print(filepath, DS)
+    show_plot(DS, filepath, 1, '2005-1-17')
 
-
-DS = xr.open_dataset(filepath)  # extract data set from netCFD file
-#------------------------------------------------------------------------
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
-datlst = []
-varlst = []
-sel = []
-
-
-OK_button = tk.Button(frame,text = "OK",
-                      command = root.destroy)
-OK_button.pack(side = BOTTOM)
-#select = int(input("Selection: "))
-
-root.mainloop()
-
-substance = sel[-1]
-"""
-
-var = Select_pollutant()
-#var = getattr(DS, substance)
-
-
-
-# ------------------------------------------
-root = tk.Tk()
-root.wm_title("Embedding in Tk")
-
-fig = Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
-
-canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-toolbar = NavigationToolbar2Tk(canvas, root)
-toolbar.update()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-
-def on_key_press(event):
-    print("you pressed {}".format(event.key))
-    key_press_handler(event, canvas, toolbar)
-
-
-canvas.mpl_connect("key_press_event", on_key_press)
-
-
-def _quit():
-    root.quit()     # stops mainloop
-    root.destroy()  # this is necessary on Windows to prevent
-                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
-
-
-button = tk.Button(master=root, text="Quit", command=_quit)
-button.pack(side=tk.BOTTOM)
 
 tk.mainloop()
 # If you put root.destroy() here, it will cause an error if the window is
