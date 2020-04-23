6bf56ccea188c7ea9977ed56611d5c7d901a705d
jakob <jakob.schoser@aerodelft.nl>
2020-04-23 11:52:24 +0200

    Changed settings in map plotter

--- Country Group/map_plot.py
+++ Country Group/map_plot.py
@@ -35,12 +35,12 @@ shown in a map
 
 summer = True   # used to select between pollution data for January and July
 
-poll_coll = "Soot.24h"  # the collection name for pollution (first part of the .nc4 filename)
+poll_coll = "O3.24h"  # the collection name for pollution (first part of the .nc4 filename)
 
 # the chemicals to be taken into account for pollution and emissions, respectively. These need to be the names of the
 # data sets inside the .nc4 files you selected
-poll_chemical = "AerMassBC"
-em_chemical = "BC"
+poll_chemical = "SpeciesConc_O3"
+em_chemical = "NO2"
 
 # the altitude levels over which emissions will be considered (available from 0 to 32). Check Altitude_levels.txt for
 # conversion to km. Level 8: 1 km altitude, level 32: 13 km altitude
@@ -50,7 +50,7 @@ emission_levels = slice(0, 32)
 # they make it impossible to see any differences between the other countries
 outliers = []  # ["Iraq", "Israel", "Latvia"]
 
-mode = ct.PLOT_POLLUTION  # the statistic which is plotted (emissions, pollution or ratio between them)
+mode = ct.PLOT_RATIO  # the statistic which is plotted (emissions, pollution or ratio between them)
 method = ct.METHOD_AVG  # the way that the data is combined inside one country (median or area-weighted average)
 
 show_spatial_analysis_map = False  # whether a second figure with spatial autocorrelation indicators should be displayed
